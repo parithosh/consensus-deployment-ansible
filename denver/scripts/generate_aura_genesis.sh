86d4d338cb39b52fbf4b20c2ca9b56223e2fa2ce
dapplion <35266934+dapplion@users.noreply.github.com>
2022-07-26 18:41:52 +0200

    Add pre-mines

--- denver/scripts/generate_aura_genesis.sh
+++ denver/scripts/generate_aura_genesis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python scripts/genesis_aura_posdao.py --config denver/scripts/genesis_config.yml > 
+python scripts/genesis_aura_posdao.py --config denver/scripts/genesis_config.yml > denver/custom_config_data/nethermind_genesis.json
