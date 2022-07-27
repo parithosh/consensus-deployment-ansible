0fbfe259f8f420d8f35dd36abacace8a9a38ae5d
dapplion <35266934+dapplion@users.noreply.github.com>
2022-07-27 11:35:15 +0200

    Fix genesis time

--- denver/scripts/generate_consensus_state.sh
+++ denver/scripts/generate_consensus_state.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Note: Must add --timestamp 9999999
+# Matching the one in denver/custom_config_data/config.yaml
+
 docker run --entrypoint=eth2-testnet-genesis \
 -v $PWD/denver/custom_config_data:/data \
 skylenet/ethereum-genesis-generator phase0 \
 --eth1-block 0x0000000000000000000000000000000000000000000000000000000000000000 \
---timestamp 1658260800 \
 --config /data/config.yaml \
 --mnemonics /data/mnemonics.yaml \
 --tranches-dir /data/tranches \
--- denver/scripts/validate_genesis_state.sh
+++ denver/scripts/validate_genesis_state.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Tool to validate
 # $network_name/custom_config_data/genesis.ssz
 
-docker run --rm \
--v $SCRIPT_DIR/../custom_config_data:/data \
-sigp/lighthouse lighthouse --testnet-dir="/data" beacon_node
+docker run --rm --network host \
+-v /home/lion/Code/gnosischain/consensus-deployment-ansible/denver/custom_config_data:/data \
+sigp/lighthouse lighthouse --testnet-dir="/data" beacon_node \
+  --http --http-address=0.0.0.0 --http-port=4000
